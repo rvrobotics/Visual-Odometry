dfad5386a0a2568e43fb02b5cce4b2528a0083fe
tushar vaidya <tushar5610@gmail.com>
2015-05-16 00:26:41 +0530

    this code works well publisesh new_vo topic but it only search T with addition of delta T

--- src/calibration1.py
+++ src/calibration1.py
@@ -45,7 +45,7 @@ y_g = []
 z_g = []
 
 def plot_ground_truth():
-	f = open('/home/rrc/catkin_ws/src/viso2-hydro/data/dataset/poses/01.txt','r')
+	f = open('/home/mahindra/tushar/06.txt','r')
 	a=[]
 	for row in f :
 	    a.append([float(x) for x in row.split()])
@@ -55,25 +55,29 @@ def plot_ground_truth():
             z_g.append(r[11])
 
 def save_result_text():
-	f = open('/home/rrc/catkin_ws/src/viso2-hydro/data/odometry_01.txt' , 'w')
+	f = open('/home/rrc/catkin_ws/src/viso2-hydro/data/odometry_06.txt' , 'w')
 	f.writelines(str(x[i])+" "+str(y[i])+" "+str(z[i])+"\n"for i in range(0,len(x)))
 	f.close()
 
 if __name__ == '__main__':
 	rospy.init_node("displacement")
 	rospy.Subscriber("/stereo_odometer/odometry", Odometry , print_position)
-	# rospy.Subscriber("/encoder" ,Odometry, print_position1)
+	rospy.Subscriber("/new_vo" ,Odometry, print_position1)
 	#rospy.Subscriber("/fix" ,Odometry, print_position2)
 							
 	rospy.spin()
-#	plot_ground_truth()
+	plot_ground_truth()
 #	save_result_text()
-	pl.plot(z , x, 'r',[-0.3075,1.8896,2.522,3.4,2.832,1.906,-.686] , [-0.0873,-0.5973,-0.36619,-1.3018,-2.098,-2.5384,-1.966] ,'g')
+	#pl.plot(z , x, 'r',[-0.3075,1.8896,2.522,3.4,2.832,1.906,-.686] , [-0.0873,-0.5973,-0.36619,-1.3018,-2.098,-2.5384,-1.966] ,'g')
 	pl.xlabel('z coordinate')
 	pl.ylabel('x coordinate')
 	pl.text( 60, .025,'KITTI DATASET \n green-> ground truth , red-> visual odometry')
+	#pl.show()
+        pl.plot(x_e, z_e,'-',x,z,'-',x_g,z_g,'-')
 	pl.show()
-	# pl.plot(x_e, y_e)
-	# pl.show()
+	#pl.plot(x,z)
+	#pl.show()
+	#pl.plot(x_g,y_g)
+	#pl.show()
 	#pl.plot(latitude,longitude)
 	#pl.show()	
--- src/compute_homography.py
+++ src/compute_homography.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python  
+'''
+This code generates homography matrix using feature corrospondance and
+compare it with homography matrices ffor R and T of Visual Odometry.
+It also does correction in Visual Odometry and publish it on new topic.
+
+Author:Tushar Vaidya
+Contect:mail@tusharvaidya.info
+'''
 import roslib
 import rospy
 import math
@@ -9,7 +17,34 @@ import geometry_msgs.msg
 import numpy as np
 from sensor_msgs.msg import PointCloud2, Image
 from message_filters import Subscriber , TimeSynchronizer
+import random
+from nav_msgs.msg import Odometry
 #from common import draw_str
+
+H_tracking = np.array([])
+H_roi = np.array([])
+
+R_vo = np.array([])
+T_vo = np.array([])
+
+new_vo_pub = rospy.Publisher("/new_vo",Odometry)
+
+def print_position(a):
+	
+	
+	(X, Y, Z) = (a.pose.pose.position.x ,\
+			a.pose.pose.position.y ,\
+			a.pose.pose.position.z )	
+	(x,y,z,w) = (a.pose.pose.orientation.x, \
+			a.pose.pose.orientation.y, \
+			a.pose.pose.orientation.z, \
+			a.pose.pose.orientation.w)
+	
+	global T_vo
+ 	T_vo = np.array([X,Y,Z])
+	global R_vo
+	R_vo = np.array([x,y,z,w])
+	
 
 lk_params = dict( winSize  = (19, 19),
                   maxLevel = 2,
@@ -56,8 +91,9 @@ def callback(l_image, r_image):
 	    if len(p0) < 4:
 		self.p0 = None
 		return
-	    H, status = cv2.findHomography(p0, p1, (0, cv2.RANSAC)[use_ransac], 10.0)
-	    print H
+	    global H_tracking
+	    H_tracking, status = cv2.findHomography(p0, p1, (0, cv2.RANSAC)[use_ransac], 10.0)
+	    #print H
 	else:
             p = cv2.goodFeaturesToTrack(frame_gray, **feature_params)
         frame0 = frame.copy()
@@ -68,32 +104,74 @@ def callback(l_image, r_image):
 #	    gray1 = frame_gray
 
 def compute_H():
-	(x,y,z,w) = rot
-	(tx,ty,tz) = trans
+	(x,y,z,w) = R_vo 
+	(tx,ty,tz) = T_vo
 	R = np.array([[1-y*y , x*y - w*z, x*z+w*y],\
 		      [x*y+w*z ,1-(x*x+w*z), y*x-w*x ],\
 		      [x*z-w*y, y*z+w*x, -(x*x+y*y)]])
         t = np.array([tx , ty, tz])
 	n = np.array([0,1,0])
 	d = 0.5
-	H = R + t* n.transpose()/d
+	global H_roi
+	H_roi = R + t* n.transpose()/d
 	#print H
 
+def correct_rnt(H1,H2,R,T,n):
+	T_good =np.array([])
+	dt=1.35
+
+	error_min =100
+	for i in range(100):
+		T_new = random.uniform(0,0.5)
+		(x,y,z,w) =  R_vo
+		R = np.array([[1-y*y , x*y - w*z, x*z+w*y],\
+		      	      [x*y+w*z ,1-(x*x+w*z), y*x-w*x ],\
+		      	      [x*z-w*y, y*z+w*x, -(x*x+y*y)]])
+		H = R + (T_vo+T_new) * n.transpose() /1.35 
+	#	print np.linalg.det(np.subtract(H1,H2))
+	#	print  error_min
+		if error_min > np.linalg.det(np.subtract(H1,H)):
+			T_good = T_new
+			error_min =  np.linalg.det(np.subtract(H1,H))
+	if not T_good:
+		return "NULL"
+	else:	
+		global new_vo_pub
+		s = Odometry()
+		s.pose.pose.position.x = T_vo[0] +T_good
+		s.pose.pose.position.y = T_vo[1] +T_good
+		s.pose.pose.position.z = T_vo[2] +T_good
+		s.pose.pose.orientation.x = R_vo[0]
+		s.pose.pose.orientation.y = R_vo[1]
+		s.pose.pose.orientation.z = R_vo[2]
+		s.pose.pose.orientation.w = R_vo[3]
+		new_vo_pub.publish(s)
+		return T_vo+T_good
+		
+	
+	
+	
+			
 if __name__ == '__main__':
     rospy.init_node('homography')
-
     listener = tf.TransformListener()
-
     #rospy.Subscriber("/surface_normals",PointCloud2,callback)
-
     rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
-        try:
-            (trans,rot) = listener.lookupTransform('/start', '/kitti_stereo', rospy.Time(0))
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-        	continue
+       # try:
+       #     (trans,rot) = listener.lookupTransform('/start', '/kitti_stereo', rospy.Time(0))
+       # except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+       # 	continue
+	
+	rospy.Subscriber("/stereo_odometer/odometry", Odometry , print_position)
 	tss = TimeSynchronizer([Subscriber("/kitti_stereo/left/image_rect", Image),Subscriber("/kitti_stereo/right/image_rect", Image)],10)
 	tss.registerCallback(callback)
-	compute_H()
+	if T_vo.any():
+		compute_H()
+	#print H_tracking
 	#print("\n")
+	#print H_roi
+	#print("\n")
+	if H_tracking.any():
+		print correct_rnt(H_tracking, H_roi, R_vo, T_vo ,np.array([0,1,0]))
 	rate.sleep()
